518b8e7ca5312ded24766161e01906b40cc14562
Lyubomila Petkova <Lyubomila.Petkova@KPMG.co.uk>
2020-12-29 12:55:51 +0000

    Add books grid

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
-import random
 
 
 app = Flask(__name__)
@@ -23,7 +22,6 @@ mongo = PyMongo(app)
 @app.route("/get_books")
 def get_books():
     books = mongo.db.books.find()
-    random.shuffle(books)
     return render_template("books.html", books=books)
 
 @app.route("/register", methods=["GET", "POST"])
