c43fdd6c26b039059e514a9bf5fb531171a7ae73
Lyubomila Petkova <Lyubomila.Petkova@KPMG.co.uk>
2021-01-01 15:49:12 +0000

    Add get book profile functionality

--- app.py
+++ app.py
@@ -106,9 +106,10 @@ def add_book():
 
 @app.route("/get_book_profile/<book_id>", methods=["GET"])
 def get_book_profile(book_id):
+    book_id = book_id
     find_book_id = mongo.db.books.find_one({"_id": ObjectId(book_id)})
     books = list(mongo.db.books.find())
-    return render_template("book_profile.html", find_book_id=find_book_id, books=books)
+    return render_template("book_profile.html", find_book_id=find_book_id, books=books, book_id = book_id)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
