09216fc84e579386a760d56b917e8de1cc81926f
Lyubomila Petkova <Lyubomila.Petkova@KPMG.co.uk>
2021-01-08 12:03:48 +0000

    Changing comments section

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def get_book_profile(book_id):
     books = list(mongo.db.books.find())
     return render_template("book_profile.html", find_book_id=find_book_id, books=books, book_id = book_id, check_comments = check_comments)
 
-@app.route("/add_comment/<doc_id>", methods=["GET", "POST"])
+@app.route("/add_comment/<book_id>", methods=["GET", "POST"])
 def add_comment(book_id):
     if request.method == "POST":
         dook_id = book_id
@@ -127,9 +127,9 @@ def add_comment(book_id):
         query = {"comments": {"$exists": True}}
         check_comments = mongo.db.books.find(query)
         if check_comments: 
-            mongo.db.books.update({"_id": ObjectId(doc_id)}, { "$addToSet": { comments: {rate_comment}}})
+            mongo.db.books.update({"_id": ObjectId(book_id)}, { "$addToSet": { comments: {rate_comment}}})
         else: 
-            mongo.db.books.update({"_id": ObjectId(doc_id)}, { "$set": { comments: {rate_comment}}})
+            mongo.db.books.update({"_id": ObjectId(book_id)}, { "$set": { comments: {rate_comment}}})
         flash("Rate/Comment Successfully Added")
         return redirect(url_for("get_book_profile"))
     return render_template("book_profile.html", find_book_id=find_book_id, books=books, book_id = book_id, check_comments = check_comments)
