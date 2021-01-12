f9e8747281be5c0f5bb97e97bc0f9b868dcf95ea
Lyubomila Petkova <Lyubomila.Petkova@KPMG.co.uk>
2021-01-12 20:47:36 +0000

    Adding validation

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def get_book_profile(book_id):
     book_id = book_id
     find_book_id = mongo.db.books.find_one({"_id": ObjectId(book_id)})
     book_name = find_book_id.get("book_name")
-    check_comments = mongo.db.comments.find("book_name": book_name)
+    check_comments = mongo.db.comments.find({"book_name": book_name})
     books = list(mongo.db.books.find())
     return render_template("book_profile.html", find_book_id=find_book_id, books=books, book_id = book_id, check_comments = check_comments)
 
